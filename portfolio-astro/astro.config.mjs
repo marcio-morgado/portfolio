63ca5ead4635063f441109d31721ba06d675c248
Marcio Morgado <marciomorgado@live.ca>
2025-07-03 01:37:12 -0400

    setup missing tailwind config

--- portfolio-astro/astro.config.mjs
+++ portfolio-astro/astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import partytown from '@astrojs/partytown';
+import partytown from "@astrojs/partytown";
 
-import relativeLinks from 'astro-relative-links';
+import relativeLinks from "astro-relative-links";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown(), relativeLinks()]
+    integrations: [partytown(), relativeLinks()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
