84e8d8427ef8dd1789b6a53b3961f3cd6dea8a1b
Marcio Morgado <marciomorgado@live.ca>
2025-07-03 00:28:21 -0400

    added relative-links package for better build output

--- portfolio-astro/astro.config.mjs
+++ portfolio-astro/astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import partytown from '@astrojs/partytown';
 
+import relativeLinks from 'astro-relative-links';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown()]
+  integrations: [partytown(), relativeLinks()]
 });
