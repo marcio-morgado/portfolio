9189c9d05116b28bb634793f600889c95bb6f094
Marcio Morgado <marciomorgado@live.ca>
2025-08-11 23:43:12 -0400

    Refactored code to import data from json file & completed spotlight section

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,19 @@ import relativeLinks from "astro-relative-links";
 
 import tailwindcss from "@tailwindcss/vite";
 
-
+import devtoolsJson from "vite-plugin-devtools-json";
 
 // https://astro.build/config
 export default defineConfig({
+    output: "static",
+
+    image: {
+        layout: "constrained",
+    },
+
     integrations: [partytown(), relativeLinks()],
+
     vite: {
-        plugins: [tailwindcss()],
+        plugins: [tailwindcss(), devtoolsJson()],
     },
 });
