b26fea771e2ac0153175e264f12b76f74ee8677a
Marcio Morgado <marciomorgado@live.ca>
2025-08-14 18:31:19 -0400

    Added MDX support, ignored all images within assets and added default node config to fix VS Code WSL mode.

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 import devtoolsJson from "vite-plugin-devtools-json";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
     output: "static",
@@ -17,7 +19,7 @@ export default defineConfig({
         layout: "constrained",
     },
 
-    integrations: [partytown(), relativeLinks()],
+    integrations: [partytown(), relativeLinks(), mdx()],
 
     vite: {
         plugins: [tailwindcss(), devtoolsJson()],
